16d5bba53d3ca4c98bb35b7bd8a896d904443d5b
Srijan Manandhar <srijan.manandhar@gmail.com>
2016-09-06 22:37:05 +0300

    Used grunt as task runner for the application

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('myblog', [])
+.controller('PostController', function(){
+	var posts=[
+	{
+		name:'First',
+		tag:'first',
+		date:'2016-09-05',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+		comment:''
+	},
+	{
+		name:'First',
+		tag:'first',
+		date:'2016-09-05',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+		comment:''
+	},
+	{
+		name:'First',
+		tag:'first',
+		date:'2016-09-05',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+		comment:''
+	},
+	{
+		name:'First',
+		tag:'first',
+		date:'2016-09-05',
+		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+		comment:''
+	}
+	];
+	this.posts = posts;
+});
