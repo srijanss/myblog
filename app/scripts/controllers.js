9133e282adda6f1e1c7e5cc18a9de26249e74551
Srijan Manandhar <srijan.manandhar@gmail.com>
2016-09-12 22:06:55 +0300

    Used json-server to provide data which can be accesed from angular app

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,5 +2,11 @@
 
 angular.module('myblog')
 .controller('PostController', ['$scope', 'postFactory', function($scope, postFactory){
-	$scope.posts = postFactory.getPosts();
+	$scope.posts = [];
+	postFactory.getPosts()
+	.then(
+		function(response) {
+			$scope.posts = response.data;
+		}
+		);
 }]);
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,45 +1,19 @@
 'use strict';
 
 angular.module('myblog')
-.factory('postFactory', function() {
-	var posts=[
-	{
-		name:'First',
-		tag:'first',
-		date:'2016-09-05',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-		comment:''
-	},
-	{
-		name:'First',
-		tag:'first',
-		date:'2016-09-05',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-		comment:''
-	},
-	{
-		name:'First',
-		tag:'first',
-		date:'2016-09-05',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-		comment:''
-	},
-	{
-		name:'First',
-		tag:'first',
-		date:'2016-09-05',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-		comment:''
-	}
-	];
+.constant('baseURL', 'http://localhost:3000/')
+.factory('postFactory', ['$http', 'baseURL', function($http, baseURL) {
+	
 	var postfac = {};
 
 	postfac.getPosts = function() {
-		return posts;
+		// return posts;
+		return $http.get(baseURL+"posts");
 	};
 
 	postfac.getPost = function(post_index) {
-		return posts[post_index];
+		// return posts[post_index];
+		return $http.get(baseURL+"posts/"+post_index);
 	};
 	return postfac;
-});
+}]);
